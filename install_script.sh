6a2f71cb0504bb5704e9fef194b0e85ab07bc813
ChasonPickles <harshbindra@live.ca>
2021-01-19 03:39:38 -0500

    - add script that allows you to compile only flink-streaming-java instead of whole repo

---
+++ install_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$HOME/flink" || exit
+
+sudo mvn -T 1C install -Dcheckstyle.skip -Drat.skip=true -DskipTests -Dfast -Dmaven.javadoc.skip=true -pl flink-streaming-java -nsu #-o
+
+cd "$HOME/flink/flink-dist" || exit
+
+sudo mvn -T 1C install -Dcheckstyle.skip -Drat.skip=true -DskipTests -Dfast -Dmaven.javadoc.skip=true -N
+
+cd "$HOME/flink" || exit
+
+sudo mvn -T 1C install -Dcheckstyle.skip -Drat.skip=true -DskipTests -Dfast -Dmaven.javadoc.skip=true -N
