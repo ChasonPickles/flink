0da6d2ea65b2e52854dd4a4229fb553d61f95d8b
ChasonPickles <harshbindra@live.ca>
2021-02-07 03:12:23 -0500

    - add print statement to install_script

--- install_script.sh
+++ install_script.sh
@@ -12,4 +12,5 @@ cd "$HOME/flink" || exit
 
 sudo mvn -T 1C install -Dcheckstyle.skip -Drat.skip=true -DskipTests -Dfast -Dmaven.javadoc.skip=true -N
 
+echo "Copying build-target to ~/flink-binary"
 cp -r build-target/* ~/flink-binary
