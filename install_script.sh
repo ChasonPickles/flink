284b5401c5e592b71aa320de8f334ae1301809b7
ChasonPickles <harshbindra@live.ca>
2021-02-08 15:10:31 -0500

    - modify install script to not overwrite conf file since we need more memory on flink

--- install_script.sh
+++ install_script.sh
@@ -13,4 +13,6 @@ cd "$HOME/flink" || exit
 sudo mvn -T 1C install -Dcheckstyle.skip -Drat.skip=true -DskipTests -Dfast -Dmaven.javadoc.skip=true -N
 
 echo "Copying build-target to ~/flink-binary"
-cp -r build-target/* ~/flink-binary
+# cp -r build-target/* ~/flink-binary
+rsync -av build-target/* ~/flink-binary --exclude=conf/flink-conf.yaml
+
